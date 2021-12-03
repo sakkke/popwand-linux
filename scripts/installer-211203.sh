002417f54d8c44c8d352ee214af78aaa253c20f8
sakkke <w32w64@gmail.com>
2021-12-03 22:13:48 +0900

    add installer

---
+++ scripts/installer-211203.sh
@@ -0,0 +1,39 @@
+timedatectl set-ntp true
+
+parted /dev/sda << /parted
+mklabel gpt
+mkpart ESP fat32 1MiB 551MiB
+set 1 esp on
+/parted
+
+mkfs.fat -F32 /dev/sda1
+mkfs.ext4 /dev/sda1mkfs
+
+mount /dev/sda2 /mnt
+mkdir /mnt/boot
+mount /dev/sda1 /mnt/boot
+
+pacstrap /mnt base dhcpcd efibootmgr grub linux linux-firmware
+
+genfstab -U /mnt/etc/fstab >> /mnt/etc/fstab
+
+arch-chroot /mnt << /arch-chroot
+ln -sf /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
+hwclock --systohc
+
+sed -Ei 's/^#(ja_JP.UTF-8 UTF-8)/\1/' /etc/locale.gen
+locale-gen
+cat >> /etc/vconsole.conf << /cat
+KEYMAP=jp106
+/cat
+
+echo earth > /etc/hostname
+ln -s /usr/lib/systemd/system/dhcpcd.service /etc/systemd/system/multi-user.target.wants/
+
+echo root:toor | chpasswd
+
+grub-install --efi-directory=/boot
+grub-mkconfig -o /boot/grub/grub.cfg
+/arch-chroot
+
+umount -R /mnt
