f4b61210a493738f86713fccaebe210c446cee95
sakkke <w32w64@gmail.com>
2021-12-11 16:28:30 +0900

    fix script

--- scripts/archiso-packages-211211.sh
+++ scripts/archiso-packages-211211.sh
@@ -3,7 +3,7 @@ mkdir live
 grep '^file://' packages.list | sed 's,^file://,,' | xargs -I{} cp -v {} live/
 wget -P live -i <(grep '^https://' packages.list)
 cd live
-repo-add live.db.tar.gz *.pkg.tar.zst
+repo-add live.db.tar.gz *.pkg.tar.*
 cd ..
 mv live airootfs/
 
