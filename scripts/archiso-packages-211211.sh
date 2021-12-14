2d48126b79c3aa6673e365fba95e7d775d2bb33e
sakkke <w32w64@gmail.com>
2021-12-14 19:03:25 +0900

    fix script

--- scripts/archiso-packages-211211.sh
+++ scripts/archiso-packages-211211.sh
@@ -1,8 +1,9 @@
-pacman -Sp - > packages.x86_64 < <(cat <(echo \
+pacman -Sp - > packages.tmp < <(cat <(echo \
   linux-firmware \
     | xargs -n1) \
       packages.x86_64 \
         | sort -u)
+mv -fv packages.{tmp,x86_64}
 
 mkdir live
 grep '^file://' packages.x86_64 | sed 's,^file://,,' | xargs -I{} cp -v {} live/
