3356cd32506a08f9c75ee69774cc2b4d6eee958c
sakkke <w32w64@gmail.com>
2021-12-15 18:28:13 +0900

    update script

--- scripts/archiso-packages-211211.sh
+++ scripts/archiso-packages-211211.sh
@@ -19,7 +19,16 @@ pacman -Sp - > packages.list < packages.x86_64
 
 mkdir live
 grep '^file://' packages.list | sed 's,^file://,,' | xargs -I{} -P0 -n1 cp -v {} live/
-grep '^https://' packages.list | xargs -P0 -n1 wget -P live -nv -q
+if type aria2c > /dev/null; then
+  grep '^https://' packages.list | xargs -P0 -n1 aria2c \
+    --show-console-readout false \
+    --summary-interval 0 \
+    -dlive \
+    -s4 \
+    -x4
+else
+  grep '^https://' packages.list | xargs -P0 -n1 wget -P live -nv -q
+fi
 cd live
 repo-add live.db.tar.gz *.pkg.tar.*
 : > packages.x86_64
